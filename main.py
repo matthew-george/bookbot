597012a30b1bea49a9128f17ad84a6373b92a7ee
matthew-george <matthew.george93@gmail.com>
2025-04-12 18:07:33 +0800

    Revisions

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from stats import word_count
+import sys
+
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(sys.argv[1])
     # print(text)
-    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"--- Begin report of {sys.argv[1]} ---")
     print(f"{word_count(text)} words found in the document")
     char_counts = get_char_counts(text)
     sortable_char_counts = []
@@ -12,7 +18,8 @@ def main():
     sortable_char_counts.sort(reverse=True, key=sort_on)
 
     for char in sortable_char_counts:
-        print(f"The '{char["name"]}' character was found {char["num"]} times")
+        # print(f"The '{char["name"]}' character was found {char["num"]} times")
+        print(f"{char["name"]}: {char["num"]}")
     print("--- End report ---")
 
 def get_book_text(path):
@@ -28,11 +35,6 @@ def get_char_counts(text):
         else:
             alphabet[char] = 1
     return alphabet
-
-def word_count(text):
-    words = text.split()
-    count = len(words)
-    return count
 
 def sort_on(dict):
     return dict["num"]
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(text):
+    words = text.split()
+    count = len(words)
+    return count
